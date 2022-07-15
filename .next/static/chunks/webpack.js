ba6bd002953872348eb637e0cf26a037ca943040
Aliyu Usman <usman.m1604342@st.futminna.edu.ng>
2022-07-15 04:27:31 +0100

    Adding CSS & Modules

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a4d18d799a49e513"; }
+/******/ 		__webpack_require__.h = function() { return "3bde0046e3fd5cff"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,21 @@
 import Link from "next/link";
 import React from "react";
 
+import "../src/style.module.css"
+
+// import "../src/style.css"
+
+//Above will throw the following error, to means simply use module.css or import from App component
+
+/*
+
+./src/style.css
+Global CSS cannot be imported from files other than your Custom <App>. Due to the Global nature of stylesheets, and to avoid conflicts, Please move all first-party global CSS imports to pages/_app.js. Or convert the import to Component-Level CSS (CSS Modules).
+Read more: https://nextjs.org/docs/messages/css-global
+Location: pages/index.jsx
+
+*/
+
 const Page = () => {
   return (
     <div>
